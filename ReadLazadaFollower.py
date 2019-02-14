af401550628519cb34ac55dcd34b0e04f017ed17
SJunWah <junwah1997@gmail.com>
2019-02-14 11:58:27 +0800

    Add files via upload

---
+++ ReadLazadaFollower.py
@@ -0,0 +1,30 @@
+import urllib
+import urllib.request
+import re
+from bs4 import BeautifulSoup
+import time
+import random
+import json
+import math
+import requests
+
+# 创建一个文件存储评论及其他数据
+
+targetFollowerNumber=4990
+
+myfile = open("tm_fz_gn_1_1_1.txt","a")
+stop = random.uniform(0.5,2)
+
+url = 'https://www.lazada.com.my/shop/renderApi/pcPageData?pageId=7890567&shopId=118&clientType=pc&lang=en&pageType=1'
+wbdata = requests.get(url).text
+data = json.loads(wbdata)
+followerNumber = data['result']['components']['111284']['moduleData']['followUserNumber']
+
+while followerNumber<targetFollowerNumber:
+    print(followerNumber,'<', targetFollowerNumber)
+    time.sleep(3)  
+else:
+    print(followerNumber)
+
+    
+myfile.close()
